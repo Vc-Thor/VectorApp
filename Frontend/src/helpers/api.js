72ae2539c2b86dad6918105eab142bdae5a19376
Hugo Fica <hugo.fica.saez@gmail.com>
2023-04-20 23:31:47 -0400

    update

--- Frontend/src/helpers/api.js
+++ Frontend/src/helpers/api.js
@@ -1,17 +1,17 @@
 import axios from 'axios';
 
 export const axiosUser = axios.create({
-  baseURL: 'http://35.85.65.108/:5000/api/user/',
+  baseURL: 'http://35.85.65.108:5000/api/user/',
 });
 export const axiosAuth = axios.create({
-  baseURL: 'http://35.85.65.108/:5000/api/auth/',
+  baseURL: 'http://35.85.65.108:5000/api/auth/',
 });
 export const axiosVector = axios.create({
-  baseURL: 'http://35.85.65.108/:5000/api/vector/',
+  baseURL: 'http://35.85.65.108:5000/api/vector/',
 });
 export const axiosVectorValue = axios.create({
-  baseURL: 'http://35.85.65.108/:5000/api/valueVector/',
+  baseURL: 'http://35.85.65.108:5000/api/valueVector/',
 });
 export const axiosAreaVector = axios.create({
-  baseURL: 'http://35.85.65.108/:5000/api/areaVector/',
+  baseURL: 'http://35.85.65.108:5000/api/areaVector/',
 });
